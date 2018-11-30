e2a2d1a38777aeac00f4ac07ecb03fdbd56c8b64
davidparry <davidparry@earthlink.net>
2018-11-29 20:19:36 -0600

    changed up the Spec to use it for entire workshop.

--- src/test/groovy/ExampleSpec.groovy
+++ src/test/groovy/ExampleSpec.groovy
@@ -5,7 +5,7 @@ import spock.lang.Specification
  * Test that a connection can take place.
  * Since everything depends on a Index the only way to test a connection is to call dropIndex when one is to current.
  */
-class RedisBaseSpec extends Specification {
+class ExampleSpec extends Specification {
 
     def "Can we connect, dropIndex only way"() {
         expect:
